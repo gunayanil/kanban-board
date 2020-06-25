d7ec19e633b36eb15225ed46e3aae5b3c2bccba6
gunayanil <gunayanilemre@gmail.com>
2020-06-25 18:55:43 -0400

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>Hold on, I am getting started!</p>
+    </div>
+  );
+}
+
+export default App;
