158f425ab1c2f8c193fd83c787c967e33029bb9b
gunayanil <gunayanilemre@gmail.com>
2020-06-25 21:30:20 -0400

    imported Card and Columns component to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import './App.css';
+import { Card } from './components/Card';
+import { Column } from './components/Column';
+import { MainContainer } from './styles';
 
 function App() {
   return (
-    <div className='App'>
-      <p>Hold on, I am getting started!</p>
-    </div>
+    <MainContainer>
+      <Column text='To Do'>
+        <Card text='Build a kanban board' />
+      </Column>
+      <Column text='In Propgress'>
+        <Card text='Survive in 2020!' />
+      </Column>
+      <Column text='Done'>
+        <Card text='Graduate (what a timing)' />
+      </Column>
+    </MainContainer>
   );
 }
 
