5c2d63d0dc558cec80e4df38fecd5f4aa91aff89
gunayanil <gunayanilemre@gmail.com>
2020-06-26 11:33:41 -0400

    imported AddNewItem component and useAppState, then rendered columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
+import { useAppState } from './AppStateContext';
+import { AddNewItem } from './components/AddNewItem';
 import { Card } from './components/Card';
 import { Column } from './components/Column';
 import { MainContainer } from './styles';
 
-function App() {
+const App = () => {
+  const { state } = useAppState();
+
   return (
     <MainContainer>
-      <Column text='To Do'>
-        <Card text='Build a kanban board' />
-      </Column>
-      <Column text='In Propgress'>
-        <Card text='Survive in 2020!' />
-      </Column>
-      <Column text='Done'>
-        <Card text='Graduate (what a timing)' />
-      </Column>
+      {state.lists.map((list, i) => (
+        <Column text={list.text} key={list.id} index={i} />
+      ))}
+      <AddNewItem toggleBtnText='+ Add another list' onAdd={console.log} />
     </MainContainer>
   );
-}
+};
 
 export default App;
